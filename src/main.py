86f443e7c26c21081cb003169f0373e9940f5538
aoi-3125 <aoi.yamauchi@tcs.com>
2026-06-12 13:17:40 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 def add(a, b, c=0):
-    if not all(isinstance(x, (int, float)) for x in (a, b, c)):
-        return "error"
-        
-    if not all(0 <= x <= 10 for x in (a, b, c)):
-        return -2
+    try:
 
-    return a + b + c
+        ab = a + b
+        result = int(ab) + int(c)
+        return result
+    except:
+        return "error"
